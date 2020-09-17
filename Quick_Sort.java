8a70a3e9f467f401f13baefff872b14f67c3ff7b
ramimhossain12 <ramimhossain6242@gmail.com>
2020-09-17 22:10:57 +0600

    Quick Sort

---
+++ Quick_Sort.java
@@ -0,0 +1,46 @@
+public class Quick_Sort {
+
+    static void QuickSort(int[] arr, int low, int high) {
+        if (low > high)
+            return;
+
+        int mid = low + (high - low) / 2;
+        int pivot = arr[mid];
+        int i = low;
+        int j = high;
+        while (i <= j) {
+            while (arr[i] < pivot)
+                i++;
+            while (arr[j] > pivot)
+                j--;
+            if (i <= j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (low < j)
+
+            QuickSort(arr, low, j);
+        if (low < i) {
+            QuickSort(arr, i, high);
+            if (high > i)
+                QuickSort(arr, i, high);
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 50, 30, 10, 60, 80};
+
+        QuickSort(arr, 0, arr.length - 1);
+        System.out.println("\nAfter sort");
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(arr[i] + "\t");
+    }
+
+}
